e21d73d6eb79a8611c108fd6d0c125d04e715c2f
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-19 08:54:47 -0700

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Start a Flask web application.
-"""
+
 
 """ Creating flask application """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 """ Defining routes """
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -17,6 +19,6 @@ def hello_hbnb():
 
     return 'Hello HBNB!'
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
