a88c7955919a1b7064ef65d383c37de774271ec7
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-19 19:51:19 -0700

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+
+""" Creating flask application """
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+""" Defining routes """
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display 'Hello HBNB!'.
+
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays 'HBNB'.
+
+    """
+
+    return'HBNB'
+
+
+@app.route('/c/<text>')
+def c_with_params(text):
+    """
+    Displays 'C' followed by the value of <text>.
+
+    """
+
+    text_no_underscore = text.replace('_', ' ')
+    return 'C {}'.format(text_no_underscore)
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_with_text_params(text):
+    """
+
+
+    """
+
+    text_no_underscore = text.replace('_', ' ')
+    return 'Python {}'.format(text_no_underscore)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """
+
+
+    """
+
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
